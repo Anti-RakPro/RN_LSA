1cda5f98606a9b10e1246a0d7a6989224102dc46
Anti-RakPro <kvit.d.o@gmail.com>
2024-12-20 14:32:22 +0200

    add:

--- App.tsx
+++ App.tsx
@@ -1,29 +1,33 @@
-
 import {NavigationContainer} from "@react-navigation/native"
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import WelcomeReactNative from "./screens/WelcomeReactNative.tsx";
 import Home from "./screens/Home.tsx";
+import {Text} from "react-native";
 
 
-const Stack = createNativeStackNavigator()
-
+const Stack = createNativeStackNavigator({
+    screens: {
+        WelcomeReactNative: WelcomeReactNative,
+    },
+})
 
 
 function App(): React.JSX.Element {
 
 
-  return (
-      <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name={'Home'} component={Home}/>
-            <Stack.Screen name={'WelcomeReactNative'} component={WelcomeReactNative}/>
+    return (
+        <WelcomeReactNative/>
+        // <NavigationContainer>
+        //     <Stack.Navigator>
+        //         <Stack.Screen name={'Home'} component={WelcomeReactNative}/>
+        //         {/*<Text>text</Text>*/}
+        //         {/*<Stack.Screen name={'Home'} component={Home}/>*/}
+        //     </Stack.Navigator>
+        // </NavigationContainer>
 
-          </Stack.Navigator>
-      </NavigationContainer>
 
-  );
+    );
 }
-
 
 
 export default App;
